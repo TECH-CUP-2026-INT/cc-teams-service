373624c725b8c321ceab3684800133f5c038ffe3
jsebastianBeltran2002 <johan.beltran-g@mail.escuelaing.edu.co>
2026-07-07 23:29:08 -0500

    chore: initial project structure with spring initializr

---
+++ src/main/java/co/edu/escuelaing/techcup/teams/ServiceTeamsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.techcup.teams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceTeamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceTeamsApplication.class, args);
+	}
+
+}

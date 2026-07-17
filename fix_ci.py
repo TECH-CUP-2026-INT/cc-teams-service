cfc9d58d6a137721aab5ed986736a68f91790b74
Juan David Rangel Jimenez <juan.rangel-j@mail.escuelaing.edu.co>
2026-07-17 16:55:45 -0500

    fix: switch from GHCR to ACR for Docker registry

--- fix_ci.py
+++ fix_ci.py
@@ -2,23 +2,23 @@ import sys
 with open(sys.argv[1], encoding='utf-8') as f:
     content = f.read()
 
-# Current broken state has both blocks - remove the incomplete first one
-old = """          az containerapp update \\
-            --name teams-service \\
-            --resource-group techcup \\
-            IMAGE_LOWER=$(echo "ghcr.io/${{ github.repository }}:latest" | tr '[:upper:]' '[:lower:]')
-          az containerapp update \\
-            --name teams-service \\
-            --resource-group techcup \\
-            --image "$IMAGE_LOWER\""""
+# Replace all GHCR registry references with ACR
+content = content.replace(
+    "REGISTRY: ghcr.io\n  IMAGE_NAME: ${{ github.repository }}",
+    "REGISTRY: techcuptournaments.azurecr.io\n  IMAGE_NAME: cc-teams-service"
+)
 
-new = """          IMAGE_LOWER=$(echo "ghcr.io/${{ github.repository }}:latest" | tr '[:upper:]' '[:lower:]')
-          az containerapp update \\
-            --name teams-service \\
-            --resource-group techcup \\
-            --image "$IMAGE_LOWER\""""
+content = content.replace(
+    "registry: ${{ env.REGISTRY }}\n          username: ${{ github.actor }}\n          password: ${{ secrets.GITHUB_TOKEN }}",
+    "registry: ${{ env.REGISTRY }}\n          username: techcuptournaments\n          password: ${{ secrets.ACR_PASSWORD }}"
+)
 
-content = content.replace(old, new)
+# Fix deploy step image reference
+content = content.replace(
+    'IMAGE_LOWER=$(echo "ghcr.io/${{ github.repository }}:latest" | tr \'[:upper:]\' \'[:lower:]\')',
+    'IMAGE_LOWER="${{ env.REGISTRY }}/cc-teams-service:latest"'
+)
+
 with open(sys.argv[1], 'w', encoding='utf-8') as f:
     f.write(content)
-print("Fixed!")
+print("Fixed to use ACR!")
